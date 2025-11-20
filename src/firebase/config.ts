d27f224aa9510c6c80c502788013657a5add9826
Tommy Lyons Travel's <tommylyons.realestate@gmail.com>
2025-11-20 16:55:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6967403383-a8bb0",
+  "appId": "1:753615298002:web:690b8e4c7f832b688ebc0b",
+  "apiKey": "AIzaSyAwsGKZlB3TxVsMpZ_XB--bi211lU3mwtg",
+  "authDomain": "studio-6967403383-a8bb0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "753615298002"
+};
