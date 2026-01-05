7e9ab65c3fe037e6bed990eb5ad51dd51150fa2f
Tommy Lyons Travel's <tommylyons.realestate@gmail.com>
2026-01-05 18:56:14 +0000

    FirebaseError: Firebase Storage: No default bucket found. Did you set th

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:753615298002:web:690b8e4c7f832b688ebc0b",
   "apiKey": "AIzaSyAwsGKZlB3TxVsMpZ_XB--bi211lU3mwtg",
   "authDomain": "studio-6967403383-a8bb0.firebaseapp.com",
+  "storageBucket": "studio-6967403383-a8bb0.appspot.com",
   "measurementId": "",
   "messagingSenderId": "753615298002"
 };
